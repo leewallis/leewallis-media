a0340b981a9353f3d5718013954d041db8b6b418
leewallis <leewallis@gmail.com>
2019-11-17 21:41:34 +1000

    Bug fixes for gallery listing

--- src/components/PhotoGallery/Galleries.js
+++ src/components/PhotoGallery/Galleries.js
@@ -3,12 +3,21 @@ import React from 'react';
 // import { PhotoGalleryContainer } from '../../styles';
 import gallery from '../../pages/Photography/GalleryContent.json';
 
+// Gallery stuff for laterzzzz
+// const galleryImages = props.galleryData.content_list.map((contentList, index) => {
+//   return <img src={contentList.filename} alt="" key={index} />;
+// });
+
+// return galleryImages;
+
 function SetupGalleryContainers(props) {
-  const galleryImages = props.galleryData.content_list.map((contentList, index) => {
-    return <img src={contentList.filename} alt="" key={index} />;
-  });
-  
-  return galleryImages;
+  return (
+    <div>
+      <h3>{props.galleryData.title}</h3>
+      <h4>{props.galleryData.sub_title}</h4>
+      <img src={props.galleryData.cover_image} alt="" />
+    </div>
+  );
 }
 
 function Galleries(props) {
