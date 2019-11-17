e5542c5b4f7abfefd8d618f7e43cc07e55e90b05
leewallis <leewallis@gmail.com>
2019-11-17 21:22:42 +1000

    A few updates including initial gallery setup and some SEO page titles

--- src/components/PhotoGallery/Galleries.js
+++ src/components/PhotoGallery/Galleries.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 // import { PhotoGalleryContainer } from '../../styles';
-// import gallery from '../../pages/Photography/gallery_content.json';
+import gallery from '../../pages/Photography/GalleryContent.json';
+
+function SetupGalleryContainers(props) {
+  const galleryImages = props.galleryData.content_list.map((contentList, index) => {
+    return <img src={contentList.filename} alt="" key={index} />;
+  });
+  
+  return galleryImages;
+}
 
 function Galleries(props) {
+  const galleryListing = gallery.project.albums.list
+    .filter(list => list.year === props.year)
+    .map((list, index) => {
+      return <SetupGalleryContainers key={index} year={props.year} galleryData={list} />
+  });
+
   return (
     <div>
-      {props.year}
+      <h2>{props.year}</h2>
+      {galleryListing}
     </div>
   );
 }
--- src/components/PhotoGallery/PhotoGallery.js
+++ src/components/PhotoGallery/PhotoGallery.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
 import { PhotoGalleryContainer } from '../../styles';
-import gallery from '../../pages/Photography/gallery_content.json';
+import gallery from '../../pages/Photography/GalleryContent.json';
 import { Galleries } from './Galleries';
 
 function PhotoGallery() {
-  //console.log(gallery.project.settings);
-
   const years = gallery.project.settings.years;
 
-  // for (let year in years){
-  //   if(years.hasOwnProperty(year)){
-  //     console.log(`${years[year]}`)
-      
-  //   }
-  // }
-
-  const yearContainer = Object.keys(years).map( (year, index) =>
+  const yearContainer = Object.keys(years).reverse().map((year, index) =>
     <Galleries key={index} year={year} />
   );
 
