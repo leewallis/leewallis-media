62702ed35783dfe177292daeefbab34d72e4e21c
Lee Wallis <leewallis@The-Jenkinz-Pro.gateway>
2020-02-09 13:07:14 +1000

    Lazy loading

--- src/components/PhotoGallery/GalleryElements.js
+++ src/components/PhotoGallery/GalleryElements.js
@@ -13,9 +13,9 @@ function GalleryElements(props) {
   const galleryAssets = props.galleryAssets.content_list
     .map((assets, index) => {
       return (
-        <GalleryElementsAssetCol key={index} xs={6} sm={6} md={4}>
-          <LazyLoad>
-            <GalleryAsset src={mediaCDN + '/' + props.galleryAssets.year + assets.filename}/>
+        <GalleryElementsAssetCol key={index} xs={10} sm={6} md={4} xsOffset={1} smOffset={0}>
+          <LazyLoad throttle={200}>
+            <GalleryAsset src={mediaCDN + '/' + props.galleryAssets.year + assets.filename + '?v=' + Math.floor(Math.random() * 1000) + 1}/>
           </LazyLoad>
         </GalleryElementsAssetCol>
       );
